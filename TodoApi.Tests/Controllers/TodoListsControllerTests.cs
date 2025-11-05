43014d9d954876a0e6b49a6cd75c831dbd46e2a8
Ignacio Badell <tbadell@gmail.com>
2025-11-05 20:44:15 +0000

    Update namespaces

--- TodoApi.Tests/Controllers/TodoListsControllerTests.cs
+++ TodoApi.Tests/Controllers/TodoListsControllerTests.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Controllers;
+using TodoApi.Data;
+using TodoApi.Dtos.Requests;
 using TodoApi.Models;
 
 namespace TodoApi.Tests;
@@ -65,7 +67,7 @@ public class TodoListsControllerTests
 
             var result = await controller.PutTodoList(
                 3,
-                new Dtos.UpdateTodoList { Name = "Task 3" }
+                new UpdateTodoList { Name = "Task 3" }
             );
 
             Assert.IsType<NotFoundResult>(result);
@@ -84,7 +86,7 @@ public class TodoListsControllerTests
             var todoList = await context.TodoList.Where(x => x.Id == 2).FirstAsync();
             var result = await controller.PutTodoList(
                 todoList.Id,
-                new Dtos.UpdateTodoList { Name = "Changed Task 2" }
+                new UpdateTodoList { Name = "Changed Task 2" }
             );
 
             Assert.IsType<OkObjectResult>(result);
@@ -100,7 +102,7 @@ public class TodoListsControllerTests
 
             var controller = new TodoListsController(context);
 
-            var result = await controller.PostTodoList(new Dtos.CreateTodoList { Name = "Task 3" });
+            var result = await controller.PostTodoList(new CreateTodoList { Name = "Task 3" });
 
             Assert.IsType<CreatedAtActionResult>(result.Result);
             Assert.Equal(3, context.TodoList.Count());
