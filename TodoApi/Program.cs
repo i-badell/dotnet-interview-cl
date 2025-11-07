0475a5bd18ad662a5f811df2d5f113ef362ab0ca
Ignacio Badell <tbadell@gmail.com>
2025-11-07 13:28:44 +0000

    Added swaggergen

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Data;
 using TodoApi.Repositories;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder
@@ -8,6 +9,10 @@ builder
 		opt.UseSqlServer(builder.Configuration.GetConnectionString("TodoContext"))
 	)
 	.AddEndpointsApiExplorer()
+	.AddSwaggerGen(c =>
+	{
+		c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Version = "v1" });
+	})
 	.AddControllers();
 
 builder.Services.AddScoped<ITodoListsRepository, TodoListsRepository>();
@@ -18,5 +23,7 @@ builder.Logging.AddConsole();
 var app = builder.Build();
 
 app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
 app.MapControllers();
 app.Run();
